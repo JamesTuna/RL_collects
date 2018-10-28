52f63089dae614f418830ed21251ff64e4b5a4b5
JamesTuna <dzhuocheng@gmail.com>
2018-10-28 20:52:05 +0800

    more demo added

--- PPO/PPO_test.py
+++ PPO/PPO_test.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 from PPO import *
 
+# 'CartPole-v0', 'MountainCar-v0'
+GAME = 'LunarLander-v2'
 epsilon = 0.1
 GAMMA = 0.99
 BATCH = 256
@@ -10,22 +12,36 @@ ACTOR_ITER = 10
 CRITOR_ITER = 2
 A_LR = 0.001
 C_LR = 0.1
-
 		
 
 if __name__ == '__main__':
 
 	RENDER = False
 	rewards = [[],[]]
-	#env = gym.make('CartPole-v0')
-	#env = gym.make('MountainCar-v0')
-	env = gym.make('LunarLander-v2')
+
+	env = gym.make(GAME)
+
 	env.seed(1)    
 	env = env.unwrapped
 	agent = PPO(n_actions=env.action_space.n,n_features=env.observation_space.shape[0],
     			a_lr=A_LR,c_lr=C_LR,name='PPO_agent')
-	agent.actor.net.load_state_dict(torch.load('./models/lander_rwd236.19130072672505_actor'))
-	agent.critor.net.load_state_dict(torch.load('./models/lander_rwd236.19130072672505_critor'))
+	
+	if GAME == 'LunarLander-v2':
+		# for lunarlander demo
+		agent.actor.net.load_state_dict(torch.load('./models/lander_rwd236.19130072672505_actor'))
+		agent.critor.net.load_state_dict(torch.load('./models/lander_rwd236.19130072672505_critor'))
+	elif GAME == 'CartPole-v0':
+		# for cartPole demo
+		agent.actor.net.load_state_dict(torch.load('./models/step5000_actor'))
+		agent.critor.net.load_state_dict(torch.load('./models/step5000_critor'))
+	elif GAME == 'MountainCar-v0'
+		# for mountain car demo
+		agent.actor.net.load_state_dict(torch.load('./models/mtcar_step-116.1643907627642_actor'))
+		agent.critor.net.load_state_dict(torch.load('./models/mtcar_step-116.1643907627642_critor'))
+	else:
+		print('game unknown')
+		exit()
+
 	running_avg = None
 	ep = 1
 	while(True):
